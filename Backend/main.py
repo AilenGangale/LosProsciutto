793645c347308d8773ca3741a030515a631839d6
AilenGangale <agangale@fi.uba.ar>
2024-07-05 20:34:13 -0300

    creacion de base de datos y conexiones

--- Backend/main.py
+++ Backend/main.py
@@ -1,22 +1,49 @@
 
-from flask import Flask, render_template
-from models import db, Cliente, Pizza, Orden
+from flask import Flask, render_template, request, jsonify
+from models import db, configure_database, Cliente, Pizza, Orden
 
-configure_database(app)
-
-app = Flask(__name__)
-# Configurar la base de datos
+app = Flask(__name__, template_folder="Frontend/templates", static_folder="Frontend")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:111276@localhost:5432/papaspizza'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+configure_database(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello world'
 
-@app.route('/hello')
-def otro_hello():
-    return render_template('index.html')
+#Devolver todos los clientes
+@app.route('/clientes/', methods=['GET'])
+def all_clientes():
+    try:
+        # Recupera los registros de una tabla
+        clientes = Cliente.query.all()
+        
+        clientes_data = []
+        #Guarda la informacion de clientes en la lista como diccionario de cada cliente
+        for cliente in clientes:
+            cliente_data = {
+                "id_cliente": cliente.id,
+                "nombre": cliente.nombre,
+                "plata": cliente.plata
+            }
+            clientes_data.append(cliente_data)
+        #Lo pone en formato de json (usa base de datos)
+        return jsonify(clientes_data)
+    except:
+        return jsonify({"error": "No se pudieron recuperar los datos"})
+    
+def data(id_conejo):
+  conejo= Conejo.query.get(id_conejo)
+  conejo_data ={
+    "id" : conejo.id,
+    "nombre": comejo.nombre,
+    "plata ": conejo.plata
+  }
+  return conejo_data
+  
+
+      
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
